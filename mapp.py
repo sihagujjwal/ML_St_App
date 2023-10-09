e59d187aa7ebe599d06468af8a302432791c7f68
sihagujjwal <145316692+sihagujjwal@users.noreply.github.com>
2023-10-09 20:44:03 +0530

    Add files via upload

--- mapp.py
+++ mapp.py
@@ -30,8 +30,8 @@ original_columns = [
 
 # Load the saved model and preprocessors
 model = tf.keras.models.load_model('mushroom_model.h5')
-scaler = joblib.load('scaler.pkl')
-encoder = joblib.load('encoder.pkl')
+scaler = joblib.load('mscaler.pkl')
+encoder = joblib.load('mencoder.pkl')
 
 st.title('Mushroom Edibility Classifier')
 
