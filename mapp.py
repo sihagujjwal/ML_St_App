d91fd38d8724db81b06acf55074e05b382f0c9e0
sihagujjwal <145316692+sihagujjwal@users.noreply.github.com>
2023-10-09 20:00:13 +0530

    Add files via upload

--- mapp.py
+++ mapp.py
@@ -29,120 +29,87 @@ original_columns = [
 
 
 # Load the saved model and preprocessors
-model = tf.keras.models.load_model('lending_model.h5')
-scaler = joblib.load('mscaler.pkl')
-encoder = joblib.load('mencoder.pkl')
+model = tf.keras.models.load_model('mushroom_model.h5')
+scaler = joblib.load('scaler.pkl')
+encoder = joblib.load('encoder.pkl')
+
+st.title('Mushroom Edibility Classifier')
 
 
 def user_input_features():
-    st.subheader("Loan Term")
-    term = st.selectbox('Term', ['36 month', '60 month'])
+    st.subheader("Cap Features")
+    cap_shape = st.selectbox('Cap Shape', ['bell', 'conical', 'convex', 'flat', 'knobbed', 'sunken'])
+    cap_surface = st.selectbox('Cap Surface', ['fibrous', 'grooves', 'scaly', 'smooth'])
+    cap_color = st.selectbox('Cap Color', ['brown', 'yellow', 'white', 'gray', 'red', 'pink', 'cinnamon', 'purple', 'buff', 'green'])
 
-    st.subheader("Loan Grade")
-    grade = st.selectbox('Loan Grade', ['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+    bruises = st.selectbox('Bruises', ['yes', 'no'])
+    odor = st.selectbox('Odor', ['almond', 'anise', 'creosote', 'fishy', 'foul', 'musty', 'none', 'pungent', 'spicy'])
 
-    st.subheader("Loan SubGrade")
-    sub_grade = st.selectbox('Loan Sub Grade', ['B3', 'A4', 'B5', 'B4', 'A1', 'B2', 'C1', 'A5', 'C2', 'A2', 'B1', 'A3', 'D2', 'C3', 'D3', 'C4', 'C5', 'D4', 'D5', 'D1', 'E1', 'E2', 'E3', 'E4', 'E5', 'F1', 'F2','F3', 'F4',
-'F5', 'G1', 'G3', 'G4', 'G2', 'B3', 'A4', 'B5', 'B4', 'A1', 'B2', 'C1', 'A5', 'C2', 'A2', 'B1', 'A3', 'D2', 'C3', 'D3', 'C4', 'C5', 'D4', 'D5', 'D1', 'E1', 'E2', 'E3', 'E4', 'E5', 'F1', 'F2', 'F3', 'F4', 'F5', 'G1',
-'G3', 'G4', 'G2', 'G5'])
+    st.subheader("Gill Features")
+    gill_attachment = st.selectbox('Gill Attachment', ['attached', 'descending', 'free', 'notched'])
+    gill_spacing = st.selectbox('Gill Spacing', ['close', 'crowded'])
+    gill_size = st.selectbox('Gill Size', ['broad', 'narrow'])
+    gill_color = st.selectbox('Gill Color', ['black', 'brown', 'gray', 'pink', 'white', 'chocolate', 'purple', 'red', 'buff', 'green', 'yellow', 'orange'])
 
-    st.subheader("Employement length")
-    emp_length = st.selectbox('emp length', ['10+ years', '2 years', '3 years', '< 1 year', '4 years', '5 years', '1 years', '6 years', '7 years', '8 years', '9 years'])
+    st.subheader("Stalk Features")
+    stalk_shape = st.selectbox('Stalk Shape', ['enlarging', 'tapering'])
+    stalk_root = st.selectbox('Stalk Root', ['bulbous', 'club', 'cup', 'equal', 'rhizomorphs', 'rooted', 'missing'])
+    stalk_surface_above_ring = st.selectbox('Stalk Surface Above Ring', ['fibrous', 'scaly', 'silky', 'smooth'])
+    stalk_surface_below_ring = st.selectbox('Stalk Surface Below Ring', ['fibrous', 'scaly', 'silky', 'smooth'])
+    stalk_color_above_ring = st.selectbox('Stalk Color Above Ring', ['brown', 'buff', 'cinnamon', 'gray', 'orange', 'pink', 'red', 'white', 'yellow'])
+    stalk_color_below_ring = st.selectbox('Stalk Color Below Ring', ['brown', 'buff', 'cinnamon', 'gray', 'orange', 'pink', 'red', 'white', 'yellow'])
 
-    st.subheader("Home Ownership")
-    home_ownership = st.selectbox('Home Ownership', ['RENT', 'MORTGAGE', 'OWN'])
+    st.subheader("Veil and Ring Features")
+    veil_type = st.selectbox('Veil Type', ['partial', 'universal'])
+    veil_color = st.selectbox('Veil Color', ['brown', 'orange', 'white', 'yellow'])
+    ring_number = st.selectbox('Ring Number', ['none', 'one', 'two'])
+    ring_type = st.selectbox('Ring Type', ['cobwebby', 'evanescent', 'flaring', 'large', 'none', 'pendant', 'sheathing', 'zone'])
 
-    st.subheader("Verification Status")
-    verification_status = st.selectbox('Verification Status', ['Verified', 'Source Verified', 'Not Verified'])
-
-    pymnt_plan = '9952'                                 
-                                       
-    st.subheader("Purpose")
-    purpose = st.selectbox('Purpose', ['debt_consolidation', 'credit_card', 'other', 'home_improvement', 'small_business', 'major_purchase', 'car', 'wedding', 'medical', 'moving', 'house', 'vacation', 'renewable_energy'])                      
-
-    st.subheader("Enter below values")
-    loan_amnt = st.text_input("Enter loan amount in digits ")
-
-    emp_title = st.text_input("Enter employee title ")                                   
-                                       
-    int_rate = st.text_input("Enter interest rate in digits ")
-
-    installment = st.text_input("Enter number of installments in digits ")
-
-    annual_inc = st.text_input("Enter annual income in digits ")
-
-    dti = st.text_input("Enter dti in digits ")
-
-    delinq_2yrs = st.text_input("Enter delinq 2yrs in digits ")
-
-    inq_last_6mths = st.text_input("Enter number of enquiry done in last 6 months in digits ")
-                                       
-    open_acc = st.text_input("Enter number of open accounts ")
-
-    pub_rec = st.text_input("Enter if any public record ")
-                                       
-    revol_bal = st.text_input("Enter if revol balance")
-
-    revol_util = st.text_input("Enter if revol util")
-
-    total_acc = st.text_input("Enter total account balance in digits")
-
-    last_pymnt_amnt = st.text_input("Enter last payment amount in digits")
-                                       
+    st.subheader("Miscellaneous Features")
+    spore_print_color = st.selectbox('Spore Print Color', ['black', 'brown', 'buff', 'chocolate', 'green', 'orange', 'purple', 'white', 'yellow'])
+    population = st.selectbox('Population', ['abundant', 'clustered', 'numerous', 'scattered', 'several', 'solitary'])
+    habitat = st.selectbox('Habitat', ['grasses', 'leaves', 'meadows', 'paths', 'urban', 'waste', 'woods'])
 
     data = {
-        'term': term,
-        'grade': grade,
-        'sub_grade': sub_grade,
-        'emp_length': emp_length,
-        'home_ownership': home_ownership,
-        'verification_status': verification_status,
-        'pymnt_plan': pymnt_plan,
-        'purpose': purpose,
-        'loan_amnt': loan_amnt,
-        'int_rate': int_rate,
-        'installment': installment,
-        'annual_inc': annual_inc,
-        'dti': dti,
-        'delinq_2yrs': delinq_2yrs,
-        'inq_last_6mths': inq_last_6mths,
-        'open_acc': open_acc,
-        'pub_rec': pub_rec,
-        'revol_bal': revol_bal,
-        'revol_util': revol_util,
-        'total_acc': total_acc,
-        'last_pymnt_amnt': last_pymnt_amnt,
-        'emp_title': emp_title
+        'cap_shape': cap_shape,
+        'cap_surface': cap_surface,
+        'cap_color': cap_color,
+        'bruises': bruises,
+        'odor': odor,
+        'gill_attachment': gill_attachment,
+        'gill_spacing': gill_spacing,
+        'gill_size': gill_size,
+        'gill_color': gill_color,
+        'stalk_shape': stalk_shape,
+        'stalk_root': stalk_root,
+        'stalk_surface_above_ring': stalk_surface_above_ring,
+        'stalk_surface_below_ring': stalk_surface_below_ring,
+        'stalk_color_above_ring': stalk_color_above_ring,
+        'stalk_color_below_ring': stalk_color_below_ring,
+        'veil_type': veil_type,
+        'veil_color': veil_color,
+        'ring_number': ring_number,
+        'ring_type': ring_type,
+        'spore_print_color': spore_print_color,
+        'population': population,
+        'habitat': habitat
     }
 
     return pd.DataFrame(data, index=[0])
 
-#if "user_data" not in st.session_state:
-#    st.session_state.user_data = user_input_features()
+if "user_data" not in st.session_state:
+    st.session_state.user_data = user_input_features()
 
+if st.button("Predict"):
+    user_data_encoded = pd.get_dummies(st.session_state.user_data, drop_first=True)
+    missing_cols = set(original_columns) - set(user_data_encoded.columns)
+    for col in missing_cols:
+        user_data_encoded[col] = 0
+    user_data_encoded = user_data_encoded[original_columns]
+    user_data_scaled = scaler.transform(user_data_encoded)
 
-def main():
-    st.title('Lending Risk Prediction')
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Lending Risk Prediction App </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    data = user_input_features()
-    result=""
-    if st.button("Predict"):
-        user_data_encoded = pd.get_dummies(data, drop_first=True)
-        user_data_scaled = scaler.transform(user_data_encoded)
+    prediction = model.predict(user_data_scaled)
+    result = encoder.inverse_transform([int(prediction[0])])
 
-        prediction = model.predict(user_data_scaled)
-        result = encoder.inverse_transform([int(prediction[0])])
-
-        st.write(f"The mushroom is predicted to be {result[0]}.")
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
-
-if __name__=='__main__':
-    main()
+    st.write(f"The mushroom is predicted to be {result[0]}.")
 
